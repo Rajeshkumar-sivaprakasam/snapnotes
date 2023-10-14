5ed1b485f00022ab45bcfe6d485a39adcfc563ea
rajesh <rk.sivaprakasam@outlook.com>
2023-10-14 23:16:10 +0530

    notes added with only header

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,46 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import React from "react";
+import { ArrowLeft } from "lucide-react";
+import { UserButton } from "@clerk/nextjs";
+import { Separator } from "@/components/ui/separator";
+type Props = {};
+
+const DashboardPage = (props: Props) => {
+  return (
+    <>
+      <div className="grainy max-h-screen">
+        <div className="max-w-7xl mx-auto p-10">
+          <div className="h-14"></div>
+          <div className="flex justify-center items-center md:flex-row flex-col">
+            <div className="flex items-center">
+              <Link href={"/"}>
+                <Button className="bg-green-600" size="sm">
+                  <ArrowLeft className="mr-1 w-3 h-3" />
+                  Back
+                </Button>
+              </Link>
+              <div className="w-4"></div>
+              <div className="text-3xl font-bold text-gray-900">My Notes</div>
+              <div className="w-4"></div>
+              <UserButton />
+            </div>
+          </div>
+        </div>
+
+        <div className="h-8"></div>
+        <Separator />
+        <div className="h-8"></div>
+        {/* List of all notes */}
+        {/* todo: conditionally render by data exisit */}
+        <div className="text-center">
+          <div className="text-xl text-gray-500 ">
+            You have no notes to render
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DashboardPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import TypeWriterTitle from "@/components/ui/TypeWriterTitle";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-
+import Link from "next/link";
+import { ArrowRight } from "lucide-react";
 export default function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-r from-rose-100 to-teal-100 grainy">
@@ -14,6 +15,13 @@ export default function Home() {
         <h1 className="font-semibold text-center text-slate-600 text-3xl">
           <TypeWriterTitle />
         </h1>
+        <div className="mt-8"></div>
+        <Link href={"/dashboard"} className="flex justify-center">
+          <Button className="bg-green-600">
+            Get Started
+            <ArrowRight className="ml-2 w-5 h-5" strokeWidth={3} />
+          </Button>
+        </Link>
       </div>
     </div>
   );
